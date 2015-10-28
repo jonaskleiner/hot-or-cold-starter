1b1131731680bef3818465631643999f919e577e
Jonas Kleiner <kleinerdesign@gmail.com>
2015-10-27 17:30:48 -0700

    defined game reqs

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
   	/*--- Hide information modal box ---*/
@@ -14,4 +13,12 @@ $(document).ready(function(){
 
 });
 
-
+// Clicking “New Game” should trigger the JavaScript function that starts a new game without refreshing or reloading page.
+// generate secret number between 1 and 100
+// user feedback --> 50 or further away from the secret number, “Ice cold”
+// user feedback --> Between 30 and 50, “cold”
+// user feedback --> Between 20 and 30, "warm"
+// user feedback --> Between 10 and 20, "hot" 
+// user feedback --> Between 1 and 10, “very hot”
+// track # of guesses until they guess the number
+// write code that ensures that the user has supplied a numeric input between 1 and 100
