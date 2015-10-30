d30bad77c70a35d6b229f83c815e570919abf6c0
Jonas Kleiner <kleinerdesign@gmail.com>
2015-10-30 14:55:32 -0700

    grab raw form value

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 
 $(document).ready(function(){
+
+	var userNum = 0; // declare user var first so it is avail inside/outside functions.
+
+	function processInput() {
+		event.preventDefault(); // prevents form from refreshing page
+		var process_string = $("#userGuess").val();  // raw form value
+		console.log(process_string); // display raw form value in console
+	}
+
+	$('form').on('submit', processInput);
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -13,7 +23,8 @@ $(document).ready(function(){
 
 });
 
-// Clicking “New Game” should trigger the JavaScript function that starts a new game without refreshing or reloading page.
+// grab user's guess, an integer, from form
+// write code that ensures that the user has supplied a numeric input between 1 and 100
 // generate secret number between 1 and 100
 // user feedback --> 50 or further away from the secret number, “Ice cold”
 // user feedback --> Between 30 and 50, “cold”
@@ -21,4 +32,4 @@ $(document).ready(function(){
 // user feedback --> Between 10 and 20, "hot" 
 // user feedback --> Between 1 and 10, “very hot”
 // track # of guesses until they guess the number
-// write code that ensures that the user has supplied a numeric input between 1 and 100
+// Clicking “New Game” should trigger the JavaScript function that starts a new game without refreshing or reloading page.
