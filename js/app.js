f8c9124d37c40d13ee34cb1e3d02d350170f1a1d
Jonas <kleinerdesign@gmail.com>
2015-10-31 12:05:35 -0700

    logic now holds onto randNum until game is reset

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 
 $(document).ready(function(){
 
-	var userNum = 0; // declare vars first so it is avail inside/outside functions.
-	var randNum = 0; // declare vars 
+	var userNum; // declare vars first so it is avail inside/outside functions.
+	var randNum = Math.floor(Math.random() * 100 +1); // create random number between 1 and 100
+
 
 	function processInput(event) {
 		event.preventDefault(); // prevents form from refreshing page
 		var process_string = $("#userGuess").val();  // raw form value
-		console.log(process_string); // display raw form value in console
+		console.log("raw user input string is: " + process_string); // display raw form value in console
 		var userNum = parseInt(process_string, 10); // convert string to integer
-		console.log("user input is " + userNum); // display converted string
+		console.log("user input converted to integer is " + userNum); // display converted string
 		$("#userGuess").val('');
 
 		if ( isNaN( userNum )) {
@@ -27,14 +28,13 @@ $(document).ready(function(){
 			console.log("user number, " + userNum + " is good to go");
 			playGame(userNum);
 		};
-		//playGame(userNum);
 	}
 
 	function playGame(val) {
 		var userNum = val;
-		var randNum = Math.floor(Math.random() * 100 +1); // create random number between 1 and 100
-		console.log("passed user number is: " + userNum);  // display random number in console
-		console.log("random number is: " + randNum);  // display random number in console
+		// if randNum exists keep it
+		console.log("randNum was generated and is: " + randNum); // display random number in console
+		console.log("passed user number is: " + userNum);  // display user number in console
 		if (userNum === randNum) {
 			$('#feedback').remove();
 			$('.game').prepend('<h2 id="feedback">Winner winner chicken dinner.</h2>');
@@ -45,15 +45,13 @@ $(document).ready(function(){
 			// currently the game logic only checks to see if the user and random nums are equal or not. 
 			// Note: the game does not reset at this point. The game is accepting new inputs. 
 			// It needs to prevent reset of randNum for the game to work and properly compare the 2 vars.
-			// currently the function newGame is called only when the top right button is pressed
-
 		}
 	}
 
 	function newGame() {
 		$("#userGuess").val(''); // reset form field
-		var userNum = 0;
-		var randNum = 0;
+		userNum = 0;
+		randNum = Math.floor(Math.random() * 100 +1); // create random number between 1 and 100
 		$('#feedback').remove();
 		$('.game').prepend('<h2 id="feedback">Make your guess</h2>');
 	}
@@ -66,7 +64,7 @@ $(document).ready(function(){
 // convert raw value of user input to integer --DONE
 // write code that ensures that the user has supplied a numeric input between 1 and 100  --DONE
 // generate secret number between 1 and 100   --DONE
-// pass userNum var from function processInput() to function playGame()
+// pass userNum var from function processInput() to function playGame() --DONE
 // user feedback --> 50 or further away from the secret number, “Ice cold”
 // user feedback --> Between 30 and 50, “cold”
 // user feedback --> Between 20 and 30, "warm"
